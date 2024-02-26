4ef9cd6f3e75ad0a4c7e7a8970bc89bbf64a9052
legohead259 <legohead259@gmail.com>
2024-02-26 14:38:39 -0500

    Update to latest PIO starter project Update READMEs Update platformio.ini Update gitignore

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file main.cpp
+ * @brief
+ * 
+ * @author Braidan Duffy
+ * @copyright 2024
+ * 
+ * @version 1.0.0
+ */
+
+#include <Arduino.h>
+
+/**
+ * @brief This function executes once on MCU startup
+ * 
+ */
+void setup(void) {
+
+};
+
+/**
+ * @brief This function executes continually while the MCU is powered
+ * 
+ */
+void loop(void) {
+
+};
+
